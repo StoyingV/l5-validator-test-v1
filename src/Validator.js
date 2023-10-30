1c663365d94a93170eebf7000fc711fe68358c7a
Vitaly Portnov <vitport@hotmail.com>
2023-10-30 20:08:55 +0300

    fixed validator

--- src/Validator.js
+++ src/Validator.js
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 import NumberSchema from './NumberSchema.js';
 import ArraySchema from './ArraySchema.js';
 import ObjectSchema from './ObjectSchema.js';
